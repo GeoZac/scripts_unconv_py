bbd446e8a75627d8a09ee117e967f34160166cb6
George Zacharia <george.zcharia@gmail.com>
2023-11-01 06:25:01 +0530

    Bring in a json traverser

---
+++ json_traverser.py
@@ -0,0 +1,12 @@
+def traverse_json_keys(data, parent_key=""):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            new_key = f"{parent_key}.{key}" if parent_key else key
+            traverse_json_keys(value, new_key)
+    elif isinstance(data, list):
+        for index, item in enumerate(data):
+            new_key = f"{parent_key}[{index}]" if parent_key else f"[{index}]"
+            traverse_json_keys(item, new_key)
+    else:
+        if parent_key:
+            print(parent_key)
