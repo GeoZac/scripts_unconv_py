0d332169a5b3384fe99444154dcd0a90941dd29f
George Zacharia <george.zcharia@gmail.com>
2023-11-03 12:05:16 +0530

    json_traverser: Print the parent key upon recursion

--- json_traverser.py
+++ json_traverser.py
@@ -1,9 +1,11 @@
 def traverse_json_keys(data, parent_key=""):
     if isinstance(data, dict):
+        print(parent_key)
         for key, value in data.items():
             new_key = f"{parent_key}.{key}" if parent_key else key
             traverse_json_keys(value, new_key)
     elif isinstance(data, list):
+        print(parent_key)
         for index, item in enumerate(data):
             new_key = f"{parent_key}[{index}]" if parent_key else f"[{index}]"
             traverse_json_keys(item, new_key)
