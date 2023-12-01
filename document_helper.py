64cc6602da9c3d3085f18c70c66d0f4eb7b8827f
George Zacharia <george.zcharia@gmail.com>
2023-12-01 22:09:26 +0530

    document_helper: Init
    To assist me with documentation of a project

---
+++ document_helper.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def document_helper(test_file):
+    with open(test_file, "r", encoding="utf-8") as file:
+        java_code = [line.strip() for line in file.readlines()]
+    found_test = False
+    for line in java_code:
+        if "@Test" in line:
+            found_test = True
+            continue
+
+        if found_test:
+            print(line)
+            found_test = False
+
+
+if __name__ == "__main__":
+    document_helper(sys.argv[1])
