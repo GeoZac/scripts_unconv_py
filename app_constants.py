41572ccf2b6fa4868e5736b5df045c37dfaf47f0
George Zacharia <george.zcharia@gmail.com>
2024-04-18 23:23:09 +0530

    spring_app_check: Move constants to a separate file

---
+++ app_constants.py
@@ -0,0 +1,5 @@
+# Constants
+VERS_CHK = "public/status/version"
+AUTH_END = "auth/login"
+USER_SEN = "SensorSystem/UnconvUser/"
+SEN_AUTH = "SensorAuthToken/SensorSystem/"
--- spring_app_check.py
+++ spring_app_check.py
@@ -1,5 +1,6 @@
 import sys
 from app_config import *
+from app_constants import VERS_CHK, AUTH_END, USER_SEN, SEN_AUTH
 from requests import Response, get, post
 from json import dumps
 from app_utils import display_sensor_table, get_expiry_duration
