7a0ab6022e216b6d9a7e8c26c72eda3c132d6904
George Zacharia <george.zcharia@gmail.com>
2024-07-12 20:37:45 +0530

    check_gitkraken_release: Allow calling from other scripts

--- check_gitkraken_release.py
+++ check_gitkraken_release.py
@@ -2,12 +2,18 @@ import requests
 
 BASE_URL = "https://release.axocdn.com/linux/gitkraken-amd64.rpm"
 
-response = requests.head(
-    BASE_URL,
-    timeout=5,
-)
 
-resp_headers = response.headers
-print(
-    resp_headers['last-modified']
-)
+def check_gitkraken_release():
+    response = requests.head(
+        BASE_URL,
+        timeout=5,
+    )
+
+    resp_headers = response.headers
+    print(
+        resp_headers['last-modified']
+    )
+
+
+if __name__ == "__main__":
+    check_gitkraken_release()
