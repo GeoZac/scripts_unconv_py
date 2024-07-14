396fb34a0b760582aa3c2645aad768453d728c5f
George Zacharia <george.zcharia@gmail.com>
2024-07-14 11:16:43 +0530

    env_monitor_data_push: Set up method to POST data

--- env_monitor_data_push.py
+++ env_monitor_data_push.py
@@ -1,5 +1,22 @@
+import requests
+
+from env_monitor_secrets import BASE_URL, AUTH_KEY
+
+
 def env_monitor_data_push():
-    pass
+    post_url = BASE_URL + "EnvironmentalReading"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    params = {
+        "access_token": AUTH_KEY,
+    }
+    requests.post(
+        post_url,
+        data=None,
+        headers=headers,
+        params=params,
+    )
 
 
 if __name__ == "__main__":
